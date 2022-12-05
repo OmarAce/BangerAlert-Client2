5cc33bb8a1cc05a7b1ced9b4a125b5bfd53afb3c
Ace <omarace95@gmail.com>
2022-12-05 14:44:19 -0800

    fixed dependencies

--- src/content/index.js
+++ src/content/index.js
@@ -1,44 +1,30 @@
-import spotifyLogo from './spotify.png'
+import spotifyLogo from "./spotify.png";
 
-import bangerAlertLogo from './bangeralert-logos.jpeg'
+import bangerAlertLogo from "./bangeralert-logos.jpeg";
 
-import dark from './dark.png'
-import faces from './faces.jpg'
-import kanye from './mbdtf.jpg'
-import cardi from './cardi.png'
-import weekend from './w.jpg'
+import dark from "./dark.png";
+import faces from "./faces.jpg";
+import kanye from "./mbdtf.jpg";
+import cardi from "./cardi.png";
+import weekend from "./w.jpg";
 
 export default {
-    nav: {
-        name: "Spotify Buddy",
-        smName: '🎵',
-        logo: `${bangerAlertLogo}`,
-        spotifyLogo: [`${spotifyLogo}`, 'Connect'],
+  nav: {
+    name: "Spotify Buddy",
+    smName: "🎵",
+    logo: `${bangerAlertLogo}`,
+    spotifyLogo: [`${spotifyLogo}`, "Connect"],
 
-        links: [
-            'Home',
-            'About',
-            'Login',
-        ]
-    },
-    hero: {
-        image: `${dark}`,
-        image2: `${kanye}`,
-        image3: `${cardi}`,
-        image4: `${weekend}`,
-        image5: `${faces}`,
-        hook: 'Spotify. Reimagined.',
-        typical: [
-            'Lyrics. ',
-            2000,
-            'Visuals. ',
-            2000,
-            'Bangers!',
-            2000
-        ],
-        button: [
-            'Connect to Spotify',
-        ]
-
-    }
-}
+    links: ["Home", "About", "Login"],
+  },
+  hero: {
+    image: `${dark}`,
+    image2: `${kanye}`,
+    image3: `${cardi}`,
+    image4: `${weekend}`,
+    image5: `${faces}`,
+    hook: "Spotify. Reimagined.",
+    typical: ["Lyrics. ", "Visuals. ", "Bangers!"],
+    button: ["Connect to Spotify"],
+  },
+};
